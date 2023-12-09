d306fd743a92175ea8ddd86368ea8496bbb62bb9
Brian Ferri <brian.ferri@dotenv.it>
2023-12-09 12:04:08 +0100

    Update include file names to reflect day 8

--- day-8/include/_init/part1.hpp
+++ day-8/include/_init/part1.hpp
@@ -1,8 +1,8 @@
-#ifndef DAY_7_PART_1_INIT__MAIN_HPP
-#define DAY_7_PART_1_INIT__MAIN_HPP
+#ifndef DAY_8_PART_1_INIT__MAIN_HPP
+#define DAY_8_PART_1_INIT__MAIN_HPP
 
 #include <fstream>
 
 int part1(std::string input_file_path);
 
-#endif // DAY_7_PART_1_INIT__MAIN_HPP
+#endif // DAY_8_PART_1_INIT__MAIN_HPP
